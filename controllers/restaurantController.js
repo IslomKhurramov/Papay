1160dcb0de1c9a2182e8f9cdfc7b1fe70f89148b
unknown <islom.khurramov@gmail.com>
2023-06-05 15:24:12 +0900

    feat: develop Product controller logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -71,6 +71,17 @@ restaurantController.logout = (req, res) => {
   res.send("logout sahifasidasiz");
 };
 
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+  if (req.session?.member?.mb_type === "RESTAURANT") {
+    req.member = req.session.member;
+    next();
+  } else
+    res.json({
+      state: "fail",
+      message: "only authenticated members with restaurant type",
+    });
+};
+
 restaurantController.checkSessions = (req, res) => {
   if (req.session?.member) {
     res.json({ state: "succeed", data: req.session.member });

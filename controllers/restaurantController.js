821d0fc990ca26978e2f1078e513c1b9874207b2
unknown <islom.khurramov@gmail.com>
2023-06-23 20:22:30 +0900

    feat:develop admin related restauratn control page

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -120,3 +120,26 @@ restaurantController.checkSessions = (req, res) => {
     res.json({ state: "fail", message: "you are not authenticated" });
   }
 };
+
+restaurantController.validateAdmin = (req, res, next) => {
+  if (req.session?.member?.mb_type === "ADMIN") {
+    req.member = req.session.member;
+    next();
+  } else {
+    const html = `<script>
+             alert("Admin page: Permission denied!");
+             window.location.replace("/resto");
+          </script>`;
+    res.end(html);
+  }
+};
+
+restaurantController.getAllRestaurants = (req, res) => {
+  try {
+    console.log("GET: cont/getAllRestaurant");
+    res.render("all-restaurants");
+  } catch (err) {
+    console.log(`ERROR, cont/getAllRestaurant`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
--- router_bssr.js
+++ router_bssr.js
@@ -39,4 +39,10 @@ router_bssr.post(
   productController.updateChosenProduct
 );
 
+router_bssr.get(
+  "/all-restaurant",
+  restaurantController.validateAdmin,
+  restaurantController.getAllRestaurants
+);
+
 module.exports = router_bssr;

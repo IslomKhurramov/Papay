c9044184c8f0653c3396c09b65268fc32eb4c97a
unknown <islom.khurramov@gmail.com>
2023-07-22 14:04:03 +0900

    feat: fulfil develop order and orderItems api business logic

--- models/Order.js
+++ models/Order.js
@@ -34,6 +34,8 @@ class Order {
 
       //order items creation
 
+      await this.recordOrderItemsData(order_id, data);
+
       return order_id;
     } catch (err) {
       throw err;
@@ -54,6 +56,39 @@ class Order {
     } catch (err) {
       console.log(err);
       throw new Error(Definer.order_err1);
+    }
+  }
+
+  async recordOrderItemsData(order_id, data) {
+    try {
+      const pro_list = data.map(async (item) => {
+        return await this.saveOrderItemsData(item, order_id);
+      });
+      const results = await Promise.all(pro_list);
+      console.log(results);
+      return true;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async saveOrderItemsData(item, order_id) {
+    try {
+      order_id = shapeIntoMongosObjectId(order_id);
+      item._id = shapeIntoMongosObjectId(item._id);
+
+      const order_item = new this.orderItemModel({
+        item_quantity: item["quantity"],
+        item_price: item["price"],
+        order_id: order_id,
+        product_id: item["_id"],
+      });
+      const result = await order_item.save();
+      assert.ok(result, Definer.order_err2);
+
+      return "created";
+    } catch (err) {
+      throw new Error(Definer.order_err2);
     }
   }
 }
